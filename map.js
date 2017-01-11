5bf425cfc744bcfc172ea61e8d76f6775344edde
Alrick <alrick.deillon@unifr.ch>
2017-01-11 16:33:50 +0100

    add another color for the castle

--- map.js
+++ map.js
@@ -20,6 +20,13 @@ var castle = L.icon({
     iconAnchor: [12, 12],
     className: 'marker'
 });
+var castle2 = L.icon({
+    iconUrl: 'icons/castle-1.png',
+    iconSize: [24, 24],
+    iconAnchor: [12, 12],
+    className: 'marker2'
+});
 L.marker([13.83808, 37.81494], {icon: tower}).addTo(map);
 L.marker([14.09396, 35.61768], {icon: tower}).addTo(map);
 L.marker([14.94478, 37.37549], {icon: castle}).addTo(map);
+L.marker([19.91138, 32.71729], {icon: castle2}).addTo(map);

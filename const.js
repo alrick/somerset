5bc46ac7b2da9693397060eb4fb54c691b5cd10c
Alrick <alrick.deillon@gmail.com>
2017-01-12 23:21:15 +0100

    oyé

---
+++ const.js
@@ -0,0 +1,139 @@
+var data = {
+    deoord: "e74f3eb7-d8d6-11e6-b16a-3f7d8b2f5214",
+    odo: "2ea9e28a-d8e3-11e6-b16a-1bb5d129a9b5",
+    lothar: "0d2ad688-d8ff-11e6-b16a-e75545913121",
+    '': "cc73111d-d8d7-11e6-b16a-89aa9fe53353"
+};
+
+var elems = {
+    fief : {
+        1: {
+            name: 'Hameau',
+            width: 40,
+            height: 49,
+            icon: 'icons/village.png',
+            icon_v: 'icons/village_v.png',
+            soldats: 0,
+            garnison: 2,
+            chevaliers: 0
+        },
+        2: {
+            name: 'Petit Village',
+            width: 40,
+            height: 49,
+            icon: 'icons/village.png',
+            icon_v: 'icons/village_v.png',
+            soldats: 2,
+            garnison: 5,
+            chevaliers: 0
+        },
+        3: {
+            name: 'Village',
+            width: 40,
+            height: 49,
+            icon: 'icons/village.png',
+            icon_v: 'icons/village_v.png',
+            soldats: 3,
+            garnison: 5,
+            chevaliers: 0
+        },
+        4: {
+            name: 'Petite Bourgade',
+            width: 40,
+            height: 49,
+            icon: 'icons/village.png',
+            icon_v: 'icons/village_v.png',
+            soldats: 5,
+            garnison: 10,
+            chevaliers: 0
+        },
+        5: {
+            name: 'Bourgade',
+            width: 40,
+            height: 49,
+            icon: 'icons/village.png',
+            icon_v: 'icons/village_v.png',
+            soldats: 5,
+            garnison: 10,
+            chevaliers: 1
+        },
+        6: {
+            name: 'Petit Bourg',
+            width: 65,
+            height: 54,
+            icon: 'icons/bourg.png',
+            icon_v: 'icons/bourg_v.png',
+            soldats: 15,
+            garnison: 20,
+            chevaliers: 2
+        },
+        8: {
+            name: 'Bourg',
+            width: 65,
+            height: 54,
+            icon: 'icons/bourg.png',
+            icon_v: 'icons/bourg_v.png',
+            soldats: 25,
+            garnison: 40,
+            chevaliers: 4
+        },
+        10: {
+            name: 'Grand Bourg',
+            width: 65,
+            height: 54,
+            icon: 'icons/bourg.png',
+            icon_v: 'icons/bourg_v.png',
+            soldats: 35,
+            garnison: 80,
+            chevaliers: 6
+        },
+        12: {
+            name: 'Ville',
+            width: 70,
+            height: 71,
+            icon: 'icons/ville.png',
+            icon_v: 'icons/ville_v.png',
+            soldats: 50,
+            garnison: 120,
+            chevaliers: 9
+        },
+        14: {
+            name: 'Grande Ville',
+            width: 70,
+            height: 71,
+            icon: 'icons/ville.png',
+            icon_v: 'icons/ville_v.png',
+            soldats: 65,
+            garnison: 160,
+            chevaliers: 12
+        }
+    },
+    def : {
+        tourBois: {
+            name: 'Tour en bois',
+            width: 18,
+            height: 57,
+            icon: 'icons/tourBois.png'
+        },
+        tourPierre: {
+            name: 'Tour en Pierre',
+            width: 36,
+            height: 57,
+            icon: 'icons/tourPierre.png'
+        },
+        chateau: {
+            name: 'Château',
+            width: 60,
+            height: 57,
+            icon: 'icons/chateau.png'
+        }
+    },
+    magic : {
+        compotier: {
+            name: '',
+            width: 40,
+            height: 53,
+            icon: 'icons/compotier.png'
+        }
+    }
+};
--- map.js
+++ map.js
@@ -1,19 +1,13 @@
 $("#map").height(window.innerHeight-20);
 
-var data = [
-    "e74f3eb7-d8d6-11e6-b16a-3f7d8b2f5214",     // deoord
-    "2ea9e28a-d8e3-11e6-b16a-1bb5d129a9b5",     // odo
-    "cc73111d-d8d7-11e6-b16a-89aa9fe53353"      // autres
-]
-
 var map = L.map('map', {
     minZoom: 7,
     maxZoom: 7
-}).setView([21, 27], 7);
+}).setView([18.15629, 34.67285], 7);
 L.imageOverlay('somerset.png', [[0, 0], [42.28, 55.00]]).addTo(map);
 
-map.on('dblclick', function(ev) {
-    alert(ev.latlng);
+map.on('contextmenu', function(ev) {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", ev.latlng.lat.toFixed(3) + ',' + ev.latlng.lng.toFixed(3))
 });
 
 var iconSizes = {
@@ -35,14 +29,14 @@ $.each(data, function (i, v) {
         dataType: 'json',
         success: function(json) {
             $.each(json, function(index, value) {
-                createMarker(value);
+                createMarker(value, i);
             });
         }
     })
 });
 
-function createMarker(data) {
-    var marker = L.marker(data.latlng, { icon: createIcon(data.type, data.owner) });
+function createMarker(data, owner) {
+    var marker = L.marker(data.latlng, { icon: createIcon(elems[data.cat][data.type], owner, data.v) });
 
     if (data.tooltip != null && data.tooltip != '') {
         var tooltip = L.tooltip({ direction: 'bottom' });
@@ -57,10 +51,9 @@ function createMarker(data) {
     marker.addTo(map);
 }
 
-function createIcon(type, owner) {
-    var width = iconSizes[type].width;
-    var height = iconSizes[type].height;
-    var icon = '<img src="icons/' + type + '.png" width="' + width + '" height="' + height + '">';
+function createIcon(elem, owner, isV) {
+    var v = isV ? '_v' : '';
+    var icon = '<img src="' + elem.icon + v + '" width="' + elem.width + '" height="' + elem.height + '">';
     var blason = '';
     if (owner != null && owner != '') {
         blason = '<img src="blasons/' + owner + '.png" class="blason">';
@@ -68,9 +61,9 @@ function createIcon(type, owner) {
 
     return L.divIcon({
         html: icon + blason,
-        iconSize: [width, height],
-        iconAnchor: [width/2, height],
-        popupAnchor: [0, -height],
+        iconSize: [elem.width, elem.height],
+        iconAnchor: [(elem.width)/2, elem.height],
+        popupAnchor: [0, -(elem.height)],
         className: 'icon'
     });
 }
